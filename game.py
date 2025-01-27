b55ff32a9af76e45d592598b663125ef09efd3ba
enesmezanii <enesmezani1@gmail.com>
2025-01-27 14:25:44 +0100

    200

--- game.py
+++ game.py
@@ -98,7 +98,7 @@ for idx, row in cartpole_data.iterrows():
     done = int(row['done'])
     agent.train(state, action, reward, next_state, done)
 
-episodes = 100
+episodes = 200
 reward_history = []
 
 for episode in range(episodes):
